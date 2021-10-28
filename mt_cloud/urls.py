89fcf75f81575f436ab8b9b9b1385d3cd93acc6e
Viacheslav Pronov <vpronov@gmail.com>
2021-10-28 18:10:46 +0300

    Collect s ver. 10

--- mt_cloud/urls.py
+++ mt_cloud/urls.py
@@ -17,8 +17,9 @@ from django.contrib import admin
 from django.urls import include, path
 from django.conf import settings
 from django.conf.urls.static import static
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('upload_app.urls')),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) \
-    + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- upload_app/urls.py
+++ upload_app/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 from .views import my_view
 
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 urlpatterns = [
     path('', my_view, name='my-view')
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
