034dc64466415013ab8b41bb75ae73b3d15e8762
Viacheslav Pronov <vpronov@gmail.com>
2021-10-28 17:36:25 +0300

    Collect s ver.8

--- mt_cloud/settings.py
+++ mt_cloud/settings.py
@@ -133,7 +133,7 @@ STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-STATIC_ROOT = os.path.join('/app', 'static')
+STATIC_ROOT = os.path.join('BASE_DIR', 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
